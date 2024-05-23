51df80130453d6d51cb3ee97736a829945f9e62b
Deep Kothari <deepk2891@gmail.com>
2024-05-23 19:49:33 +0530

    Create our first Http Route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js"
 
 const app = express()
 
+app.get('/',(req,res) => {
+  console.log(req);
+  return res.status(234).send('welcome to mern stack tutorial')
+})
+
 app.listen(PORT,() => {
-  console.log(`App is listening to port: ${ PORT }`);
+  console.log(`App is listening to port http://localhost:${ PORT }`);
 })
