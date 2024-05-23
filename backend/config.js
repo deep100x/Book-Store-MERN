968f1bbabb90b4f938888c985e791f9a03266384
Deep Kothari <deepk2891@gmail.com>
2024-05-23 20:29:16 +0530

    Add MongoDB and mongoose to node js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 8069
+
+export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.otvwrdp.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN';
+//export const mongoDBURL = 'mongodb+srv://🟨USERNAME:🟨PASSWORD@book-store-mern.otvwrdp.mongodb.net/🟨COLLECTION-NAME?retryWrites=true&w=majority&appName=Book-Store-MERN';
