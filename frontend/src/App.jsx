9b4c6a6800702c6fc72d56fbbe3a16bd7f59d143
Deep Kothari <deepk2891@gmail.com>
2024-05-31 12:44:43 +0530

    Added Theme Switcher

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import ShowBook from "./pages/ShowBook"
@@ -6,9 +6,29 @@ import DeleteBook from "./pages/DeleteBook"
 import CreateBook from "./pages/CreateBook"
 import EditBook from "./pages/EditBook"
 
+import { FiSun, FiMoon } from "react-icons/fi"
+
 const App = () => {
+	const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark")
+
+	useEffect(() => {
+		if (theme === "dark") {
+			document.documentElement.classList.add("dark")
+		} else {
+			document.documentElement.classList.remove("dark")
+		}
+		localStorage.setItem("theme", theme)
+	}, [theme])
+
+	const toggleTheme = () => {
+		setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"))
+	}
+
 	return (
-		<div>
+		<div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-black dark:text-white pt-10 transition-all">
+			<button onClick={toggleTheme} className="bg-blue-500 text-white p-2 rounded fixed right-2 top-2">
+				{theme === "dark" ? <FiMoon className="size-5" /> : <FiSun className="size-5" />}
+			</button>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/books/details/:id" element={<ShowBook />} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -27,8 +27,8 @@ const Home = () => {
 	return (
 		<div className="p-4">
 			<div className="flex justify-center items-center gap-x-4">
-				<button className="bg-sky-300 hover:bg-sky-400 px-4 py-1 rounded-lg">Table</button>
-				<button className="bg-sky-300 hover:bg-sky-400 px-4 py-1 rounded-lg">Card</button>
+				<button className="bg-sky-300 hover:bg-sky-400 dark:bg-sky-800 dark:hover:bg-sky-500 px-4 py-1 rounded-lg">Table</button>
+				<button className="bg-sky-300 hover:bg-sky-400 dark:bg-sky-800 dark:hover:bg-sky-500 px-4 py-1 rounded-lg">Card</button>
 			</div>
 			<div className="flex justify-between items-center">
 				<h1 className="text-3xl my-8">Books List</h1>
